6ed0e2b868dc82a30afc640ee3624711c7cc5167
Caroline Hester <caheste@clemson.edu>
2018-02-20 13:49:51 -0500

    test yo

--- Lab4/Lab4main.c
+++ Lab4/Lab4main.c
@@ -22,6 +22,7 @@ int main(int argc, char *argv[])
     // This will be a pointer to the head of the tree
     //treeRoot *tree = treeConstruct();
     listRoot *list = listConstruct();
+    //yo
 
     // Array to store character frequencies
     unsigned short int frequencies[256];
